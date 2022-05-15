30cc3d3227adeb1a79a8c2e6b55971f5dff72cb7
Yessirkep <esik.95.kz@gmail.com>
2022-05-15 21:43:23 +0600

    add checkout page

--- dist/sidebar.254cde8b.js
+++ dist/sidebar.254cde8b.js
@@ -2848,7 +2848,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54149" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52466" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
